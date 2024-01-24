6777d1f2f25aedb2d9eb32f94565f12c2281118d
cld200-c13 education.cloud.sap <cld200-c13@education.cloud.sap>
2024-01-24 17:29:13 +0000

    Initial Commit @1/24/2024, 5:29:13 PM

---
+++ app/myapp/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'myapp/test/integration/FirstJourney',
+		'myapp/test/integration/pages/BooksList',
+		'myapp/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('myapp') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
